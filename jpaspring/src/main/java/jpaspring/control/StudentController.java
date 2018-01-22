88cdb6fe7480f8b92a559dd2bd35ec41a484456d
Leonardo (NPDS) <leonardo.dutra@crateus.ufc.br>
2018-01-22 15:42:08 -0200

    modularizando e melhorando exceptions

--- jpaspring/src/main/java/jpaspring/control/StudentController.java
+++ jpaspring/src/main/java/jpaspring/control/StudentController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import jpaspring.exceptions.StudentNullPointerException;
 import jpaspring.model.Student;
 
 // Classe controladora contendo cadastro(inclusão numa lista), 
@@ -27,7 +28,7 @@ public class StudentController {
 	
 	private Map<Integer, Student> students = new HashMap<Integer, Student>(); // variável e construtor do objeto students do tipo Map
 	
-	private int cumulateCountStudents = 0;
+	private int cumulateCountStudents = 0; // contador geral da lista
 	
 	// método que adiciona item à lista students
 	@RequestMapping(method = RequestMethod.POST)
@@ -37,14 +38,16 @@ public class StudentController {
 			if(students.size() > 0) { // check se tem itens na lista
 				
 				students.put(((int) students.size() + 1), student); // continua adicionando itens na lista
-				cumulateCountStudents += 1;
+				
+				cumulateCountStudents += 1; // adicionando valor no contador da lista
 				
 			} else {
 				
 				students.put((int) students.size() + 1, student); // adiciona o primeiro item quando a lista tá zerada
-				cumulateCountStudents += 1;
+				
+				cumulateCountStudents += 1; // adicionando valor no contador da lista
 
-			}
+			} // final do bloco Else que verifica se existem itens a lista
 		
 	} // final do método addStudent
 	
@@ -63,12 +66,13 @@ public class StudentController {
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public void editStudent(@RequestBody Student student, @PathVariable int id) {
 
-		student.setId((long)id);
+		student.setId((long)id); // setando ID para efetuar edição em seguida
 		
-		if(students.containsKey(id)) {
+		if(students.containsKey(id)) { // verifica se existe item naquele ID passado
 			
 			students.put(id, student); // recolocando novo objeto na mesma posição
-		}
+		
+		} // final da verificação se existe ID naquela lista
 		
 	} // final do método editStudent
 	
@@ -77,8 +81,7 @@ public class StudentController {
 	@RequestMapping(method = RequestMethod.GET)
 	public Collection<Student> listStudents(){
 		
-		// retorno da lista contendo objetos do tipo Student
-		return students.values();
+		return students.values(); // retorno da lista contendo objetos do tipo Student
 		
 	} // final do método listStudents
 	
@@ -87,7 +90,7 @@ public class StudentController {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public Student viewStudent(@PathVariable int id) {
 		
-		return students.get(id);
+		return students.get(id); // retornando o item de lista, após filtrar por ID
 		
 	} // final do método viewStudent
 	
@@ -97,17 +100,15 @@ public class StudentController {
 	@RequestMapping(value = "/", params = "filterName", method = RequestMethod.GET)
 	public List<String> searchStudent(@RequestParam("filterName") String filterName) {
 		
-		List<String> result = new ArrayList<String>(); // armazenando em lista resultado da pesquisa de cada objeto Student
-		
 		Student student = new Student(); // criando objeto para trabalharmos com cada objeto da lista Map
 		
-		// laço para varrer todos os itens da lista, visando procurar por parametro informado
-		
+		// variável para armazenar os resultados
+		List<String> result = new ArrayList<String>();
 
 		// verificando se a lista está vazia
 		if(students.isEmpty()) {
 			
-			result.add("Lista vazia");
+			result.add("Lista vazia"); // adicionando a lista notificação que a lista está vazia
 			
 		} else {
 			
@@ -120,29 +121,25 @@ public class StudentController {
 					
 					if(student.getName().contains(filterName)) { //verificação se o valor name do objeto corresponde ao valor do parametro informado
 						
-						// informando resultado positivo da busca
-						result.add("Encontrado '" + filterName + "' em: " + i);
+						result.add("Encontrado '" + filterName + "' em: " + i); // informando resultado positivo da busca
 					
 					} else {
 						
-						// informando resultado negativo da busca
-						result.add("Não encontrado em " + i);
+						result.add("Não encontrado em " + i); // informando resultado negativo da busca
 						
 					}
 					
 				} catch (NullPointerException ne) { // lançando exceção por causa de item vazio na lista 
 					
-					result.add("Índice " + i + " excluído");
+					result.add("Índice " + i + " excluído"); // adicionando à lista que um item está excluido
 					
-				}
-				
+				} // término do bloco catch
 
+			} // final do laço For que varre todos os índices da lista
 
-			}
-
-		}		
+		} // final do bloco Else que continua considerando que a lista não está vazia		
 	
-		return result;
+		return result; // retorno dos resultados obtidos até aqui nas buscas da lista
 	  
 	} // final do método buscar
 	
---
+++ jpaspring/src/main/java/jpaspring/exceptions/StudentNullPointerException.java
@@ -0,0 +1,20 @@
+package jpaspring.exceptions;
+
+public class StudentNullPointerException extends NullPointerException{
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+
+	public StudentNullPointerException(String msg) {
+		super(msg);
+		
+		throw new StudentNullPointerException("Índice excluído");
+
+	}
+
+}
