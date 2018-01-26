b465fb36aa4949cf17a556eef6e3685d738f6e51
Leonardo (NPDS) <leonardo.dutra@crateus.ufc.br>
2018-01-26 21:47:04 -0200

    melhorando e simplificando o código

--- jpaspring/src/main/java/jpaspring/control/StudentController.java
+++ jpaspring/src/main/java/jpaspring/control/StudentController.java
@@ -1,11 +1,9 @@
 package jpaspring.control;
 
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,78 +12,61 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import jpaspring.exceptions.StudentNullPointerException;
 import jpaspring.manager.StudentManager;
 import jpaspring.model.Student;
 
-// Classe controladora contendo cadastro(inclusão numa lista), 
-// atualização (modificação indexada na lista), filtro também
-// exclusão (remoção de item da lista) e listagem, bem como visualização 
 @RestController
 @RequestMapping("/api/student")
 public class StudentController {
 
 	StudentManager studentManager = new StudentManager();
 
-	// método que adiciona item à lista students
+
 	@RequestMapping(method = RequestMethod.POST)
 	public void addStudent(@RequestBody Student student) {
 				
-		studentManager.save(student, studentManager.getCumulateCountStudents()); // continua adicionando itens na lista
-				
-		studentManager.setCumulateCountStudents(+1); // adicionando valor no contador da lista
+		studentManager.save(student);
 		
-	} // final do método addStudent
+	}
 	
-	
-	// método que remove item da lista students
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public void removeStudent(@PathVariable("id") long id) {
 		
-		studentManager.delete((int) id); // remove item da lista com base no índice
+		studentManager.delete(id);
 		
-		//setCumulateCountStudents(-1);
-		
-	} // final do método removeStudent
+	} 
 	
 	
-	// método que modifica item da lista, recebendo por parâmetro um código e 
-	// este será usado como indexador de referência
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public void editStudent(@RequestBody Student student, @PathVariable int id) {
+	public void editStudent(@RequestBody Student student, @PathVariable long id) {
 		
-		studentManager.update(id, student); // recolocando novo objeto na mesma posição
+		studentManager.update(id, student);
 		
-	} // final do método editStudent
+	} 
 	
 	
-	// método que lista todos os itens da lista
 	@RequestMapping(method = RequestMethod.GET)
 	public Collection<Student> listStudents(){
 		
-		return studentManager.listAll(); // retorno da lista contendo objetos do tipo Student
+		return studentManager.listAll();
 		
-	} // final do método listStudents
+	} 
 	
 	
-	// método permite visualização do item da lista
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public Student viewStudent(@PathVariable int id) {
 		
-		return studentManager.obtainValueKey(id); // retornando o item de lista, após filtrar por ID
+		return studentManager.obtainValueKey(id); 
 		
-	} // final do método viewStudent
+	} 
 	
 	
-	// método permite buscar/filtrar item pelo atributo "name"
-	//@SuppressWarnings("unlikely-arg-type")
-	@RequestMapping(value = "/", params = "filterName", method = RequestMethod.GET)
-	public List<String> searchStudent(@RequestParam("filterName") String filterName) {
+	@RequestMapping(value = "/", params = "name", method = RequestMethod.GET)
+	public List<Student> searchStudent(@RequestParam("name") String name) {
 		
-		return studentManager.search(filterName, studentManager.getCumulateCountStudents());
+		return studentManager.search(name);
 	  
-	} // final do método searchStudent
+	} 
 	
-	
-	
-} // final da classe StudentController
+} 
--- jpaspring/src/main/java/jpaspring/manager/StudentManager.java
+++ jpaspring/src/main/java/jpaspring/manager/StudentManager.java
@@ -10,186 +10,89 @@ import jpaspring.model.Student;
 
 public class StudentManager {
 
-	private Map<Integer, Student> students = new HashMap<Integer, Student>(); // variável e construtor do objeto students do tipo Map
+	private Map<Long, Student> students = new HashMap<Long, Student>(); 
 	
-	private int cumulateCountStudents = 0; // contador geral da lista
+	private long counter = 0; // contador
 	
-	public void save(Student student, int counter) {
+	public void save(Student student) {
 		
-		// nesse caso está verificando se a chave da qual se está tentando incluir na lista 
-		// não existe e se a diferença de contagem da lista e do contador não indica uma recente exclusão na lista
-		if((!existKey((int) student.getId())) && (totalItens() == counter)) { 
+		if(!existKey(student.getId())) { 
 			
-				students.put((totalItens() + 1), student); 
+				students.put(student.getId(), student); 
+				setCounter(+1);
+				
+				System.out.println("count: " + getCounter());
 			
-			} else if((!existKey((int) student.getId())) && 
-					(totalItens() < counter) && 
-						(student.getId() <= counter)) {
-				
-					students.put((int) student.getId(), student); 
-				
-					// verificando se acumulador não tá zerado
-					if(getCumulateCountStudents() > 0) {
-						
-						setCumulateCountStudents(-1);
-						
-					} 
-				
-			} else { // adicionando novo item a lista
-				
-				students.put((int) student.getId(), student); 
-				
-			} // FINAL - adicionando novo item a lista
+			} 
 
-		} // final do método save
+		} 
 	
 	
-	public void delete(int Key) {
-		
-		// verificação com Key existente		
-		if(existKey(Key)) {
-			
-			// quando a Key a ser excluida é igual ao acumulador, indicando que é o ultimo item da lista
-			if(Key == getCumulateCountStudents()) {
-				
-				// verificando também se o item anterior a este não está excluido e que necessite ser removido do contador
-				if(existKey(Key - 1)) { 
-					
-					students.remove(Key);
-					setCumulateCountStudents(-1);
-					
-				} else { // removendo a Key atual e a anterior
-					
-					students.remove(Key);
-					students.remove(Key - 1);
-					setCumulateCountStudents(-2);
-					
-				} // FINAL - // removendo a Key atual e a anterior
-				
-			} else { // quando a Key passada é menor que o acumulador, ou seja, item do meio da lista ou inicio
-				
-				students.remove(Key);
-				
-			} // FINAL - quando a Key passada é menor que o acumulador, ou seja, item do meio da lista ou inicio
-			
-		} else { // quando a Key já foi excluida, mas ficou espaço livre sob efeito do contador
-			
-			// quando ainda há itens na lista
-			if(totalItens() > 0) {
-						
-						// loop para ir verificando itens vazios e aplicar remoção
-						do {
-						
-							students.remove(Key);
-							setCumulateCountStudents(-1);
-						
-						}while(!existKey(Key-1));
-				
-			} else { // quando não há itens na lista
-				
-				System.out.print("Sem itens a excluir");
-				
-			} // FINAL - quando não há itens na lista
-			
-		} // FINAL - quando a Key já foi excluida, mas ficou espaço livre sob efeito do contador
+	public void delete(long key) {
 
-	} // final do método delete
+		students.remove(key);
+		setCounter(-1);
+
+		System.out.println("count: " + counter);
+		
+	} 
 	
 	
-	// método para atualizar valores da lista
-	public void update(int Key, Student student) {
+	public void update(long key, Student student) {
 		
-		student.setId((long) Key); // setando ID para efetuar edição em seguida
+		student.setId(key); 
 		
-		if(existKey(Key)) {
-			students.put(Key, student); // recolocando novo objeto na mesma posição
+		if(existKey(key)) {
+			
+			students.put(key, student);
+			
 		}
 		
-	} // final do método update
+	} 
 	
 	
-	// método para retornar todos os itens da lista
 	public Collection<Student> listAll(){
 		
-		return students.values();  // retorno da lista contendo objetos do tipo Student
+		return students.values(); 
 	
-	} // final do método listAll
-	
+	} 
 
 	
-	// método para filtro/pesquisa na lista, através de parametro passado
-	public List<String> search(String filter, int counterList){
+	public List<Student> search(String filter){
 		
-		Student student = new Student(); // criando objeto para trabalharmos com cada objeto da lista Map
+		List<Student> resultStudents = new  ArrayList<>();
 		
-		// variável para armazenar os resultados
-		List<String> result = new ArrayList<String>();
 		
-		if(listIsEmpty()) {
+		for (Student s : students.values()) {
 			
-			result.add("Lista vazia"); // adicionando a lista notificação que a lista está vazia
-			
-		} else {
-			
-			// laço para percorrer todos os itens incluidos na lista até aqui
-			for(int i = 1; i <= counterList; i++) {
+			if(s.getName().contains(filter))
 				
-				try { // usando try...catch pra prevenir erro de item vazio na lista, por causa das exclusões
-					
-					student = students.get(i); // obtendo objeto individualmente para posterior comparação do For
-					
-					if(student.getName().contains(filter)) { //verificação se o valor name do objeto corresponde ao valor do parametro informado
-						
-						result.add("Encontrado '" + filter + "' em: " + i); // informando resultado positivo da busca
-					
-					} else {
-						
-						result.add("Não encontrado em " + i); // informando resultado negativo da busca
-						
-					}
-					
-				} catch (NullPointerException ne) { // lançando exceção por causa de item vazio na lista 
-					
-					result.add("ÍNDICE " + i + " EXCLUÍDO"); // adicionando à lista que um item está excluido
-					
-				} // término do bloco catch
-
-			} // final do laço For que varre todos os índices da lista
+				resultStudents.add(s);
 			
-		} // final do bloco else, quando a lista possui itens
+		}
+		
+		return resultStudents;
 
-		return result; // retorno dos resultados obtidos até aqui nas buscas da lista
+	} 
+	
+	
+	public long getCounter() {
 		
-// para auxiliar em testes		
-//		result.add("Total contador: " + counterList);
-//		result.add("total itens obtido da lista: " + totalItens());
+		return counter;
+	
+	} 
+	
+	
+	public void setCounter(long counter) {
 		
-//			for(int x = 1; x <= counterList; x++) {
-//				result.add("key" + x + " (existe) [" + obtainValueKey(x) + "]: " + existKey(x));
-//			}
-	} // final do método search
+		this.counter += counter;
+	
+	} 
 	
 	
-	// método retorna contagem atual do acumulador auxiliar da lista
-	public int getCumulateCountStudents() {
+	public boolean existKey(long key) {
 		
-		return cumulateCountStudents;
-	
-	} // final do método getCumulateCountStudents
-
-
-	// método para manipular valores do acumulador auxiliar da lista
-	public void setCumulateCountStudents(int cumulateCountStudents) {
-		
-		this.cumulateCountStudents += cumulateCountStudents;
-	
-	} // final do método setCumulateCountStudents
-	
-	
-	// método verifica se key repassada existe na lista
-	public boolean existKey(int Key) {
-		
-		if(students.containsKey(Key)) { // verifica se existe item naquele ID passado
+		if(students.containsKey(key)) { 
 			
 			return true;
 			
@@ -197,48 +100,23 @@ public class StudentManager {
 			
 			return false;
 			
-		} // final da verificação se existe ID naquela lista
+		} 
 		
-	} // final do método existKey
+	}
 	
-	
-	// método para obter valor colocado naquela Key
-	public Student obtainValueKey(int Key) {
+
+	public Student obtainValueKey(long key) {
 		
-		if(students.containsKey(Key)) { // verifica se existe item naquele ID passado
+		if(students.containsKey(key)) {
 			
-			return students.get(Key);
+			return students.get(key);
 			
 		} else {
 			
 			return null;
 			
-		} // final da verificação se existe ID naquela lista
+		} 
 		
-	} // final do método obtainValueKey	
-	
-	
-	// método para verificar se a lista está vazia
-	public boolean listIsEmpty() {
-		
-		if(students.isEmpty()) {
-			
-			return true;
-			
-		}else {
-			
-			return false;
-			
-		}
-		
-	} // final do método listIsEmpty
-	
-	
-	// método para retornar total de itens atualmente inseridos na lista
-	public int totalItens() {
-		
-		return students.size();
-		
-	} // final do método totalItens
+	} 	
 	
 }
